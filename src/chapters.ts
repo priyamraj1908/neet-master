7d6d34da4c5c9c78ddc36176d24cb521859ed287
priyamraj1908 <priyamraj19082006@gmail.com>
2026-03-12 20:43:25 +0530

    src/ chapters.ts

---
+++ src/chapters.ts
@@ -0,0 +1,57 @@
+export const neetChapters = {
+
+Physics: [
+
+"Laws of Motion",
+"Work Energy Power",
+"Rotational Motion",
+"Gravitation",
+"Thermodynamics",
+"Oscillations",
+"Waves",
+"Electrostatics",
+"Current Electricity",
+"Magnetism",
+"Electromagnetic Induction",
+"Optics",
+"Modern Physics"
+
+],
+
+Chemistry: [
+
+"Atomic Structure",
+"Periodic Table",
+"Chemical Bonding",
+"States of Matter",
+"Thermodynamics",
+"Equilibrium",
+"Redox Reaction",
+"Electrochemistry",
+"Chemical Kinetics",
+"Surface Chemistry",
+"Organic Chemistry Basics",
+"Hydrocarbons",
+"Biomolecules"
+
+],
+
+Biology: [
+
+"Cell Structure",
+"Cell Cycle",
+"Biological Classification",
+"Plant Kingdom",
+"Animal Kingdom",
+"Morphology of Flowering Plants",
+"Anatomy of Plants",
+"Human Physiology",
+"Reproduction",
+"Genetics",
+"Evolution",
+"Ecology",
+"Biotechnology"
+
+]
+
+}
