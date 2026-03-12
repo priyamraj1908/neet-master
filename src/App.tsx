6db5b973976859c438ef30225cd36414eb70822b
priyamraj1908 <priyamraj19082006@gmail.com>
2026-03-12 20:19:31 +0530

    src/ App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 export default function App() {
   return (
-    <div style={{textAlign:"center",marginTop:"50px"}}>
-      <h1>NEET Master App 🚀</h1>
-      <p>App successfully running</p>
+    <div style={{fontFamily:"Arial",padding:"40px"}}>
+      <h1>NEET Master 🚀</h1>
+
+      <h2>Subjects</h2>
+      <ul>
+        <li>Physics</li>
+        <li>Chemistry</li>
+        <li>Biology</li>
+      </ul>
+
+      <h2>Features</h2>
+      <ul>
+        <li>📚 Chapter Wise PYQ</li>
+        <li>🧠 Mind Maps</li>
+        <li>⚡ Quick Revision</li>
+        <li>📝 Mock Tests</li>
+      </ul>
+
+      <p>Welcome to your NEET preparation platform.</p>
     </div>
   );
 }
