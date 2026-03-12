f3b921a971710f84e96582867c60c3dae53416cc
priyamraj1908 <priyamraj19082006@gmail.com>
2026-03-12 20:49:37 +0530

    src/neetDatabase.ts

---
+++ src/neetDatabase.ts
@@ -0,0 +1,95 @@
+export const neetDatabase = {
+
+Physics:[
+
+{
+chapter:"Laws of Motion",
+
+topics:[
+{
+topic:"Newton Laws",
+
+pyq:[
+"2023: Force equals mass times acceleration",
+"2022: Which law explains inertia?"
+],
+
+revision:[
+"F = ma",
+"Inertia is resistance to change in motion"
+]
+
+},
+
+{
+topic:"Friction",
+
+pyq:[
+"2021: Static friction formula?",
+"2020: Limiting friction definition"
+],
+
+revision:[
+"F = μN",
+"Static friction > kinetic friction"
+]
+
+}
+
+]
+
+},
+
+{
+chapter:"Work Energy Power",
+
+topics:[
+{
+topic:"Work",
+
+pyq:[
+"2023: Work formula?",
+"2022: SI unit of work?"
+],
+
+revision:[
+"W = F × displacement",
+"Unit = Joule"
+]
+
+}
+
+]
+
+}
+
+],
+
+Biology:[
+
+{
+chapter:"Cell Structure",
+
+topics:[
+{
+topic:"Cell Organelles",
+
+pyq:[
+"2023: Mitochondria is called?",
+"2022: Ribosome function?"
+],
+
+revision:[
+"Mitochondria = powerhouse",
+"Ribosomes = protein synthesis"
+]
+
+}
+
+]
+
+}
+
+]
+
+}
