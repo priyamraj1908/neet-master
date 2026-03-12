d23522113dad8c9c570799ae1ca59bde100d6902
priyamraj1908 <priyamraj19082006@gmail.com>
2026-03-12 19:28:53 +0530

    src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{textAlign:"center",marginTop:"50px"}}>
+      <h1>NEET Master App 🚀</h1>
+      <p>App successfully running</p>
+    </div>
+  );
+}
